5dd3273816e85ab031ee225aaa5b429eb9ee5565
StevenChandranata <stevenchandranata10@gmail.com>
2024-03-17 22:05:06 +0700

    TugasWeek4

--- app/src/main/java/com/ubaya/adv160421055week4/view/StudentDetailFragment.kt
+++ app/src/main/java/com/ubaya/adv160421055week4/view/StudentDetailFragment.kt
@@ -1,21 +1,40 @@
 package com.ubaya.adv160421055week4.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.ubaya.adv160421055week4.R
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.ubaya.adv160421055week4.databinding.FragmentStudentDetailBinding
+import com.ubaya.adv160421055week4.viewmodel.DetailViewModel
 
 class StudentDetailFragment : Fragment() {
+    private lateinit var detailViewModel: DetailViewModel
+    private lateinit var binding: FragmentStudentDetailBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_detail, container, false)
+        binding = FragmentStudentDetailBinding.inflate(inflater,container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        detailViewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+
+        detailViewModel.fetch()
+
+        detailViewModel.studentLD.observe(viewLifecycleOwner, Observer { student ->
+            binding.txtID.setText(student.id)
+            binding.txtName.setText(student.name)
+            binding.txtBod.setText(student.dob)
+            binding.txtPhone.setText(student.phone)
+        })
+    }
 }
--- app/src/main/java/com/ubaya/adv160421055week4/view/studentListItemFragment.kt
+++ app/src/main/java/com/ubaya/adv160421055week4/view/studentListItemFragment.kt
@@ -16,7 +16,6 @@ class StudentListItemFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_student_list_item, container, false)
     }
 
